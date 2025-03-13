09dbd5aa4086b1635eecc40743af797ba5409a64
Diego Alonso Witrón López <maximusdiegoalonsodbz@gmail.com>
2025-03-13 17:50:58 -0600

    Se agregó el botón flotante de WhatsApp

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -163,6 +163,11 @@
         </div>
     </section>
 
+    <section class="div-flotante">
+        <a href="https://wa.me/529932787360?text=Hola, deseo información." target="_blank" class="icono-flotante">
+            <img src="{{ asset('main/images/whatsapp.png') }}" loading="lazy" alt="WhatsApp" class="icono-flotante-img">
+        </a>
+    </section>
 
 
     <!-- AOS JS y otros scripts -->
